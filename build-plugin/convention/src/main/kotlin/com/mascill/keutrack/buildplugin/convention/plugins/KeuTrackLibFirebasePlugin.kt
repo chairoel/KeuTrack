bf80179332c32c8f5326660d8516af4c937d867a
Chairoel <projectchairul@gmail.com>
2026-07-19 15:37:47 +0700

    [FIX] Align protobuf with Firestore protolite-well-known-types

--- build-plugin/convention/src/main/kotlin/com/mascill/keutrack/buildplugin/convention/plugins/KeuTrackLibFirebasePlugin.kt
+++ build-plugin/convention/src/main/kotlin/com/mascill/keutrack/buildplugin/convention/plugins/KeuTrackLibFirebasePlugin.kt
@@ -6,18 +6,13 @@ import org.gradle.api.Project
 
 /**
  * Adds base Firebase dependencies for Android library modules.
+ *
+ * Do not exclude `protolite-well-known-types`: Firestore needs types such as
+ * `com.google.type.LatLng` from that artifact at runtime.
  */
 class KeuTrackLibFirebasePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            configurations.configureEach {
-                exclude(
-                    mapOf(
-                        "group" to "com.google.firebase",
-                        "module" to "protolite-well-known-types",
-                    ),
-                )
-            }
             configureFirebaseBase()
         }
     }
